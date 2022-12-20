83a579a9d7889b33935ccdbf4f9d33f7e9b4fd31
Andre Chen <andrexchen@gmail.com>
2022-12-20 00:20:37 -0500

    TABS

--- src/chem_parser.h
+++ src/chem_parser.h
@@ -6,19 +6,19 @@
 #include <optional>
 
 namespace chem_parser {
-    std::istream &getSymbol(std::istream &i, std::string &output) {
-        output.clear();
-        
-        if (i.eof()) {
-            i.setstate(std::ios_base::failbit);
-            return i;
-        }
-        
-        output.push_back(i.get());
-        while (!i.eof() && std::islower(i.peek())) {
-            output.push_back(i.get());
-        }
-        
-        return i;
-    }
+	std::istream &getSymbol(std::istream &i, std::string &output) {
+		output.clear();
+		
+		if (i.eof()) {
+			i.setstate(std::ios_base::failbit);
+			return i;
+		}
+		
+		output.push_back(i.get());
+		while (!i.eof() && std::islower(i.peek())) {
+			output.push_back(i.get());
+		}
+		
+		return i;
+	}
 }
